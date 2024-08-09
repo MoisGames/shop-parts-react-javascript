1fb5f9fc4c1eec921dc3b3a4372390a9c9b91d44
moisgames <VLAD981010@gmail.com>
2024-08-09 22:11:57 +0700

    style PartItem.module.css

--- client/src/components/UI/PartItem/PartItem.js
+++ client/src/components/UI/PartItem/PartItem.js
@@ -3,8 +3,18 @@ import styles from './PartItem.module.css'
 
 const PartItem = ({part}) => {
     return (
-        <div>
-            item
+        <div className={styles.PartItem__container}>
+            <div className={styles.PartItem__item}>
+                <img className={styles.PartItem__logo} src={part.img}></img>
+                    <div>
+                        <div className={styles.PartItem__header}>
+                        "MASUMA"
+                        </div>
+                        <div className={styles.PartItem__name}>
+                            {part.name_parts}
+                        </div>
+                    </div>
+            </div>
         </div>
     );
 };
