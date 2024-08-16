8909633898d4b54d866360aa780b92a47c434bc0
moisgames <VLAD981010@gmail.com>
2024-08-16 09:30:29 +0700

    style Fix Width PartItem

--- client/src/components/UI/PartList/PartList.js
+++ client/src/components/UI/PartList/PartList.js
@@ -7,11 +7,11 @@ import PartItem from '../PartItem/PartItem';
 const PartList = observer(() => {
     const {part} = useContext(Context)
     return (
-        <span className={styles.partList__span}>
+        <div className={styles.partList__span}>
             {part.parts.map(part =>
                 <PartItem key={part.id} part = {part}/>
             )}
-        </span>
+        </div>
     );
 });
 
