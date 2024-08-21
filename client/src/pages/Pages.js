a5291e91012aa4e0a4013ddcd0a31c440d0a117c
moisgames <VLAD981010@gmail.com>
2024-08-21 18:52:05 +0700

    feat Ending developing Pages.js
    The pages component is responsible for pagination, there is a main button followed by a button to return to page 1 and the next one to return to page 1 of products back. If you are already on the first page, the buttons are disabled. Then the page numbers, from 1 to 8. It is calculated based on the array of the total number of pages with products that you have filtered. And at the end, the next page button and the final page (maybe the only one), which are responsible for the opposite actions Back page.

--- client/src/pages/Pages.js
+++ client/src/pages/Pages.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {Context} from '../index'
 import styles from './styles/Pages.module.css'
 import { observer } from 'mobx-react-lite';
@@ -6,31 +6,133 @@ import setPage from '../store/PartStore'
 
 const Pages = observer (() => {
     const {part} = useContext(Context)
-    const pageCount = Math.ceil(part.totalCount / part.limit) // округляем для получения количества страниц
+    const pageCount = Math.ceil(part.totalCount / part.limit) // 110 страниц округляем для получения количества страниц
     const pages = []
     
-    console.log(pages, "pages");
-    console.log(part.page, "part page");
-
-    for (let i = 0; i < pageCount; i++ ) { //массив с цифрами страницы
-        pages.push(i+1) // Номер страницы
+    for(let i = 0; i < pageCount; i++) {
+        pages.push(i + 1)
     }
+    let [beginCountSlice,setBeginCountSlice] = useState(0)
+    let [endCountSlice,setEndCountSlice] = useState(8)
+    const defaultArrayPages = [...pages.slice(beginCountSlice,endCountSlice)]
+    
+    const defaultLiClass = styles.pages__item
+    const buttonBackNextStyleDefault = styles.pages__item_button_next_back 
+    let [backDisabled, setBackDisabled] = useState(true)
+    let [nextDisabled, setNextDisabled] = useState(false)
+    
+    
 
     return (
         <nav className={styles.pages__container}>
             <ul className={styles.pages__row_container}>
-                {pages.map(page => 
-                    <li 
-                        className={styles.pages__item}
-                        key={page}
-                        active={part.page === page}
-                        onClick={() => 
-                            part.setPage(page) 
+                    <button 
+                        className={buttonBackNextStyleDefault}
+                        disabled={backDisabled}
+                        onClick={() => {
+                            setBackDisabled(true)
+                            setNextDisabled(false)
+                            setBeginCountSlice(0)
+                            setEndCountSlice(8)
+                            part.setPage(1)
+                        }}
+                        >
+                            {'<<'}
+                    </button>
+                    <button 
+                        className={buttonBackNextStyleDefault}
+                        disabled={backDisabled}
+                        onClick={() => {
+                            if (part.page  === 2) {
+                                setBackDisabled(backDisabled = true)
+                                setBeginCountSlice(beginCountSlice - 1)
+                                setEndCountSlice(endCountSlice - 1)
+                                part.setPage(part.page - 1)
+                            } else if (beginCountSlice >= 1){
+                                setBeginCountSlice(beginCountSlice - 1)
+                                setEndCountSlice(endCountSlice - 1)
+                                part.setPage(part.page - 1)
+                                setBackDisabled(false)
+                                }
+                                else {
+                                setBeginCountSlice(beginCountSlice - 1)
+                                setEndCountSlice(endCountSlice - 1)
+                                part.setPage(part.page - 1)
+                                setBackDisabled(false)
+                                setNextDisabled(false)
+                                }
                             }
+                        }
+                        >
+                            {'<'}
+                    </button>
+                    
+                
+                {defaultArrayPages.map(page => // отправляем текущую страницу в setPage
+                                    
+                    <li 
+                        className={defaultLiClass}
+                        key={page}
+                        id={page}
+                        disabled={false}
+                        onClick={(e) => {
+                            part.setPage(page)
+                        
+                            setBackDisabled(backDisabled = false)
+                            if (part.page === 1) {
+                                setBackDisabled(backDisabled = true)
+                                setNextDisabled(nextDisabled = false)
+                            }
+                            if (part.page === pageCount) {
+                                setNextDisabled(nextDisabled = true)
+                                setBackDisabled(backDisabled = false)
+                            }
+                            
+                            
+                            
+                            }
+                        }
+                        value={page}
                         >
                             {page}
                     </li>
                 )}
+                    <button 
+                        className={buttonBackNextStyleDefault}
+                        disabled={nextDisabled}
+                        onClick={() => {
+                            if (part.page === pageCount - 2) {
+                                setNextDisabled(nextDisabled = true)
+                                setBeginCountSlice(beginCountSlice + 1)
+                                setEndCountSlice(endCountSlice + 1)
+                                part.setPage(pageCount - 1)
+                            } else {
+                                setBeginCountSlice(beginCountSlice + 1)
+                                setEndCountSlice(endCountSlice + 1)
+                                part.setPage(part.page + 1)
+                                setBackDisabled(false)
+                                }
+                            }
+                            
+                        }
+                        
+                        >
+                            {'>'}
+                    </button>
+                    <button 
+                        className={buttonBackNextStyleDefault}
+                        disabled={nextDisabled}
+                        onClick={() => {
+                                setNextDisabled(true)
+                                setBackDisabled(false)
+                                setBeginCountSlice(beginCountSlice = pageCount - 8)
+                                setEndCountSlice(endCountSlice = pageCount)
+                                part.setPage(pageCount)
+                                
+                        }}
+                        >
+                            {'>>'}
+                    </button>
             </ul>
         </nav>
     );
